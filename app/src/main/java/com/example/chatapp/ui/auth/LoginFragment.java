ecde8f567b54508df853835747f3a5f4b29d89ee
xgarhontx <xgarhontx@gmail.com>
2023-04-21 10:18:34 -0700

    Login Layout

--- app/src/main/java/com/example/chatapp/ui/auth/LoginFragment.java
+++ app/src/main/java/com/example/chatapp/ui/auth/LoginFragment.java
@@ -5,12 +5,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.FragmentLoginBinding;
 
 /**
  * Default landing fragment. <br>
@@ -21,6 +23,9 @@ import com.example.chatapp.R;
  * - "register" button <br>
  */
 public class LoginFragment extends Fragment {
+
+    private FragmentLoginBinding binding;
+
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -28,14 +33,33 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        //TODO Buttons and stuff
+        //Login Button
+        binding.buttonLogin.setOnClickListener(button -> {
+            if (checkPassword()) {
+                Navigation.findNavController(getView()).navigate(
+                        LoginFragmentDirections.actionLoginToRegisterFragment()
+                );
+            }
+        }); //TODO check correct credential and say error
+
+        //Register Button
+        binding.buttonRegister.setOnClickListener(button -> {
+            Navigation.findNavController(getView()).navigate(
+                    LoginFragmentDirections.actionLoginToRegisterFragment()
+            );
+        });
+    }
+
+    private boolean checkPassword() {
+        return true; //TODO check correct credential and say error
     }
 }
