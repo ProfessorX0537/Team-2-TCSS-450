dba0e8d950ebfffb27206fc2ed88aefe321a930e
Philip <philip1fox1@gmail.com>
2023-04-24 20:32:40 -0700

    Homepage weather card & tweaks

--- app/src/main/java/com/example/chatapp/ui/main/HomeFragment.java
+++ app/src/main/java/com/example/chatapp/ui/main/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.chatapp.ui.main;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -26,4 +27,16 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().;
+//    }
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        ((AppCompatActivity)getActivity()).getSupportActionBar().show();
+//    }
 }
