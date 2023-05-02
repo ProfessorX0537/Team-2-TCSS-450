ed7c4b0a2fd8d649f04f0adc2c6e6434a8f16cd9
IAmMeers <smithl64@uw.edu>
2023-05-02 10:20:01 -0700

    Weather UI - Radio buttons switch fragments

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -27,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
     }
 
     /**
@@ -40,4 +41,6 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.toolbar, menu);
         return true;
     }
+
+
 }
