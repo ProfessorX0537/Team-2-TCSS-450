c0343ee6798f9be620fed6e0c025a97d076b42a0
Dav Hunyah <xgarhontx@gmail.com>
2023-05-15 20:49:38 -0700

    ChatList: uses ws to get list of ChatRoom associated to user

--- app/src/main/java/com/example/chatapp/ui/main/chat/chatlist/ChatListItem.java
+++ app/src/main/java/com/example/chatapp/ui/main/chat/chatlist/ChatListItem.java
@@ -7,13 +7,15 @@ public class ChatListItem implements Serializable  {
     private final String mLatestMessage;
     private final String mLatestDate;
     private final int mNotifCount;
+    private final int mRoomID;
 
-    public ChatListItem(String mRoomName, String mLatestMessage, String mLatestDate, int mNotifCount) {
+    public ChatListItem(String mRoomName, String mLatestMessage, String mLatestDate, int mNotifCount, int mRoomID) {
         //TODO parse data & format
         this.mRoomName = mRoomName;
         this.mLatestMessage = mLatestMessage;
         this.mLatestDate = mLatestDate;
         this.mNotifCount = mNotifCount;
+        this.mRoomID = mRoomID;
     }
 
     public String getmRoomName() {
---
+++ app/src/main/java/com/example/chatapp/ui/main/chat/chatlist/ChatListItemViewModel.java
@@ -0,0 +1,121 @@
+package com.example.chatapp.ui.main.chat.chatlist;
+
+import android.app.Application;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.chatapp.R;
+import com.example.chatapp.io.RequestQueueSingleton;
+import com.example.chatapp.model.UserInfoViewModel;
+import com.example.chatapp.ui.main.chat.chatroom.ChatRoomItem;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+public class ChatListItemViewModel extends AndroidViewModel {
+
+    MutableLiveData<ArrayList<ChatListItem>> mItemList;
+
+    public ChatListItemViewModel(@NonNull Application application) {
+        super(application);
+        mItemList = new MutableLiveData<>();
+    }
+
+    public void addItemListObserver(@NonNull LifecycleOwner owner,
+                                 @NonNull Observer<? super ArrayList> observer) {
+        mItemList.observe(owner, observer);
+    }
+
+    public void getChatRooms(int memberID, String jwt) {
+        String url = getApplication().getResources().getString(R.string.url_webservices) +
+                "mychats/" + memberID;
+
+        Request request = new JsonObjectRequest(
+                Request.Method.GET,
+                url,
+                null,
+                this::handleSuccess,
+                this::handleError) {
+
+            @Override
+            public Map<String, String> getHeaders() {
+                Map<String, String> headers = new HashMap<>();
+                // add headers <key,value>
+                headers.put("Authorization", jwt);
+                return headers;
+            }
+        };
+
+        request.setRetryPolicy(new DefaultRetryPolicy(
+                10_000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        //Instantiate the RequestQueue and add the request to the queue
+        RequestQueueSingleton.getInstance(getApplication().getApplicationContext())
+                .addToRequestQueue(request);
+    }
+
+    private void handleSuccess(final JSONObject response) {
+        try {
+            int rowCount = response.getInt("rowCount");
+            JSONArray rows = response.getJSONArray("result");
+            ArrayList temp = new ArrayList<>(rowCount);
+            for (int i = 0; i < rowCount; i++) {
+                JSONObject curr = rows.getJSONObject(i);
+                temp.add(new ChatListItem(
+                        curr.getString("name"),
+                        "WIP",
+                        "WIP",
+                        0,
+                        curr.getInt("chatid")
+                ));
+            }
+            mItemList.setValue(temp);
+            Log.v("ChatListItemViewModel", "mItemList: " + mItemList.getValue().toString());
+        } catch (Exception e) {
+            Log.e("ChatListItemViewModel", "Couldn't handle success:\n" + e.getMessage());
+        }
+    }
+
+    private void handleError(final VolleyError error) {
+        if (Objects.isNull(error.networkResponse)) {
+            Log.e("NETWORK ERROR", error.getMessage());
+        }
+        else {
+            String data = new String(error.networkResponse.data, Charset.defaultCharset());
+            Log.e("CLIENT ERROR",
+                    error.networkResponse.statusCode +
+                            " " +
+                            data);
+        }
+    }
+
+//    public void setupItemsList() { //TODO remove for webservice
+//        for (int i = 0; i < 10; i++) {
+//            ChatListItem curr = new ChatListItem(
+//                    "Chat Title " + i,
+//                    "This is the lastest message sent in the chat room bruv.",
+//                    "Apr " + (10 + i),
+//                    i
+//            );
+//            mItemList.add(curr);
+//        }
+//    }
+}
