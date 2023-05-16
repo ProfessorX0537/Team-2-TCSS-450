2422b899d631a77dd02426ac085c94dfb60d1d9c
IAmMeers <smithl64@uw.edu>
2023-05-16 16:50:49 -0700

    WeatherViewModel - Today view updated

--- app/src/main/java/com/example/chatapp/ui/main/weather/WeatherTodayFragment.java
+++ app/src/main/java/com/example/chatapp/ui/main/weather/WeatherTodayFragment.java
@@ -1,31 +1,95 @@
 package com.example.chatapp.ui.main.weather;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.content.res.AppCompatResources;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.chatapp.R;
+import com.example.chatapp.databinding.FragmentHomeBinding;
+import com.example.chatapp.databinding.FragmentWeatherTodayBinding;
+import com.example.chatapp.model.WeatherInfoViewModel;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link WeatherTodayFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+
 public class WeatherTodayFragment extends Fragment {
+
+    private WeatherInfoViewModel mViewModel;
+    private FragmentWeatherTodayBinding mBinding;
 
     public WeatherTodayFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mViewModel = new ViewModelProvider(getActivity()).get(WeatherInfoViewModel.class);
+        mViewModel.connectGet();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_weather_today, container, false);
+        mBinding = FragmentWeatherTodayBinding.inflate(inflater);
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mViewModel.addResponseObserver(
+                getViewLifecycleOwner(),
+                this::observeData
+        );
+
+    }
+
+    private void observeData(JSONObject result) {
+        if (result.length() != 0) {
+            try {
+                Date date = new Date();   // given date
+                Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
+                calendar.setTime(date);   // assigns calendar to given date
+                int currentHour = calendar.get(Calendar.HOUR_OF_DAY); // gets hour in 24h format
+                int currentMonth = calendar.get(Calendar.MONTH);       // gets month number, NOTE this is zero based!
+                int currentDay = calendar.get(Calendar.DAY_OF_MONTH);
+                int currentYear = calendar.get(Calendar.YEAR);
+
+                JSONObject current = result.getJSONObject("current_weather");
+                String currentTemp = Math.round(Float.parseFloat(current.getString("temperature"))) + "°F";
+                String currentDate = mViewModel.mMonthName[currentMonth] + " " + currentDay + ", " + currentYear;
+
+                int currentWeatherCode = Integer.parseInt(current.getString("weathercode"));
+                int iconID = WeatherCodes.getWeatherIconName(currentWeatherCode);
+                Drawable icon = AppCompatResources.getDrawable(getContext(), iconID);
+
+                mBinding.textTemperature.setText(currentTemp);
+                mBinding.textDaycondition.setText(WeatherCodes.getWeatherCodeName(currentWeatherCode));
+                mBinding.imageWeathercondtion.setImageDrawable(icon);
+                mBinding.textDate.setText(currentDate);
+//                Log.d("hi", temp+"");
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
