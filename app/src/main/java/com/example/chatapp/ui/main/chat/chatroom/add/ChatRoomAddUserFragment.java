5a9c2d0796bfb0a43c4f060ff69c7a63a7100203
Dav Hunyah <xgarhontx@gmail.com>
2023-05-16 19:19:41 -0700

    ChatRoomAdd: Fragment & Assets

---
+++ app/src/main/java/com/example/chatapp/ui/main/chat/chatroom/add/ChatRoomAddUserFragment.java
@@ -0,0 +1,25 @@
+package com.example.chatapp.ui.main.chat.chatroom.add;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.chatapp.R;
+
+public class ChatRoomAddUserFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chat_room_add_user, container, false);
+    }
+}
