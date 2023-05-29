69f698086e83b0be299682e6aa86133183d67484
Philip <philip1fox1@gmail.com>
2023-05-29 10:19:24 -0700

    Time updated to show min/hours/days ago

--- app/src/main/java/com/example/chatapp/utils/SimpleDate.java
+++ app/src/main/java/com/example/chatapp/utils/SimpleDate.java
@@ -1,9 +1,38 @@
 package com.example.chatapp.utils;
 
+import android.util.Log;
+
 import java.util.Date;
 
 public class SimpleDate {
     public static String stringDateFromEpochString(String epochString) {
-        return new Date((long) (Double.parseDouble(epochString) * 1000)).toString();
+
+        double epochTimeDecimal = Double.parseDouble(epochString);
+        long epochTime = (long) epochTimeDecimal;
+        long currentTime = System.currentTimeMillis()/1000;
+
+        long timeDifference = currentTime - epochTime;
+
+        long seconds = timeDifference % 60;
+        long minutes = (timeDifference / 60) % 60;
+        long hours = (timeDifference / (60 * 60)) % 24;
+        long days = timeDifference / (60 * 60 * 24);
+
+        String timeAgo = formatTimeAgo(days, hours % 24, minutes % 60, seconds % 60);
+
+        return timeAgo;
+        //return new Date((long) (Double.parseDouble(epochString) * 1000)).toString();
+    }
+
+    private static String formatTimeAgo(long days, long hours, long minutes, long seconds) {
+        if (days > 0) {
+            return days + " days ago";
+        } else if (hours > 0) {
+            return hours + " hours ago";
+        } else if (minutes > 0) {
+            return minutes + " minutes ago";
+        } else {
+            return seconds + " seconds ago";
+        }
     }
 }
