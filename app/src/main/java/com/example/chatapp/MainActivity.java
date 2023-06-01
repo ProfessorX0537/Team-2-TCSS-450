d6dd8c80a47a3315658862cacc926e658f577ab1
Dav Hunyah <xgarhontx@gmail.com>
2023-06-01 15:45:01 -0700

    Home Messages: Won't add message from curr user, won't add message if in seen chat room

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,14 +1,5 @@
 package com.example.chatapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.navigation.NavController;
-import androidx.navigation.NavDestination;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
@@ -24,17 +15,24 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDestination;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import com.example.chatapp.databinding.ActivityMainBinding;
 import com.example.chatapp.model.NewMessageCountViewModel;
 import com.example.chatapp.model.PushyTokenViewModel;
 import com.example.chatapp.model.UserInfoViewModel;
 import com.example.chatapp.services.PushReceiver;
-import com.example.chatapp.ui.main.changepass.ChangePassFragment;
 import com.example.chatapp.ui.main.chat.chatlist.ChatListItemViewModel;
 import com.example.chatapp.ui.main.chat.chatroom.ChatRoomItem;
 import com.example.chatapp.ui.main.chat.chatroom.ChatRoomItemsViewModel;
 import com.example.chatapp.ui.main.chat.chatroom.add.ChatRoomAddUserItemViewModel;
-import com.example.chatapp.ui.main.contacts.ContactCard;
 import com.example.chatapp.ui.main.contacts.ContactsViewModel;
 import com.example.chatapp.ui.main.home.HomeMessagesItem;
 import com.example.chatapp.ui.main.home.HomeMessagesItemViewModel;
@@ -42,7 +40,6 @@ import com.google.android.material.badge.BadgeDrawable;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -180,6 +177,7 @@ public class MainActivity extends AppCompatActivity {
 
     /**
      * Let user change their password.
+     *
      * @author Xavier Hines
      */
     private void changePass() {
@@ -219,7 +217,7 @@ public class MainActivity extends AppCompatActivity {
                         .get(UserInfoViewModel.class)
                         .getJwt()
         );
-   }
+    }
 
     //Action bar nav back/up
     @Override
@@ -270,17 +268,16 @@ public class MainActivity extends AppCompatActivity {
                     //If the user is not on the chat room (aka not the sender) screen, update NewMessageCountView Model
                     if (nd.getId() != R.id.chatRoomFragment && mChatRoomItemsViewModel.mChatId != intent.getIntExtra("chatId", -1)) {
                         mNewMessageModel.incrementFromChatId(intent.getIntExtra("chatId", -1));
+
+                        //Home
+                        if (!cm.getSender().equals(mUserInfo.getUsername())) {
+                            ArrayList<HomeMessagesItem> temp = mHomeMessagesItemViewModel.mHomeMessageList.getValue();
+                            temp.add(new HomeMessagesItem(cm.getMessageId(), cm.getMessage(), cm.getSender(), cm.getTimeStamp(), intent.getIntExtra("chatId", -1)));
+                            mHomeMessagesItemViewModel.getHomeMessageList().setValue(temp);
+                        }
                     }
                     //Inform the view model holding chatroom messages of the new message.
                     mChatRoomItemsViewModel.addMessage(intent.getIntExtra("chatId", -1), cm);
-
-                    ////////////////////////////////////FOR HOMEPAGE///////////////////////////////
-
-                    ArrayList<HomeMessagesItem> temp = mHomeMessagesItemViewModel.mHomeMessageList.getValue();
-                    temp.add(new HomeMessagesItem(cm.getMessageId(), cm.getMessage(), cm.getSender(), cm.getTimeStamp(), intent.getIntExtra("chatId", -1)));
-                    mHomeMessagesItemViewModel.getHomeMessageList().setValue(temp);
-
-
                 }
             } else if (intent.getAction().equals(PushReceiver.CHATLIST_INVITE)) {
                 if (intent.getStringExtra("username").equals(mUserInfo.getUsername())) { //if I got added
@@ -335,15 +332,16 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
             } else if (intent.getAction().equals(PushReceiver.CHATLIST_RENAME)) {
-                if (nd.getId() == R.id.chatRoomFragment  && intent.getIntExtra("chatId", -1) == mChatRoomItemsViewModel.mChatId) { //if in chat room
+                if (nd.getId() == R.id.chatRoomFragment && intent.getIntExtra("chatId", -1) == mChatRoomItemsViewModel.mChatId) { //if in chat room
                     mChatRoomItemsViewModel.mChatRoomName.setValue(intent.getStringExtra("chatRoomName"));
                 } else if (nd.getId() == R.id.navigation_chat) {
                     //refresh list //TODO find in list and add instead
                     mChatListItemViewModel.getChatRooms(mUserInfo.getMemberID(), mUserInfo.getJwt());
                 }
-            } else if(intent.getAction().equals(PushReceiver.CONNECTION_ADD)){
+            } else if (intent.getAction().equals(PushReceiver.CONNECTION_ADD)) {
                 Log.i("MainActivity", "onReceive: " + nd.getId());
-                if (nd.getId() == R.id.navigation_connections) { }
+                if (nd.getId() == R.id.navigation_connections) {
+                }
                 Log.i("MainActivity", "onReceive: " + nd.getId());
 
 /*                    List<ContactCard> contacts = mContactsViewModel.getContacts();
